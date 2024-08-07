21aeef15467890cfced54218f5ac26df844e5207
Chebichii-Lab <chebichiiserem@gmail.com>
2024-08-07 15:12:16 +0300

    add new model class migration

--- src/bemserver_core/model/team.py
+++ src/bemserver_core/model/team.py
@@ -9,7 +9,7 @@ class Team(AuthMixin, Base):
 
     id = sqla.Column(sqla.Integer, primary_key=True)
     name = sqla.Column(sqla.String(80), unique=True, nullable=False)
-    user_group_id = sqla.Column(sqla.ForeignKey("user_groups.id"), nullable=False)
+    user_group_id = sqla.Column(sqla.ForeignKey("u_groups.id"), nullable=False)
 
     user_group = sqla.orm.relationship(
         UserGroup,
@@ -27,6 +27,37 @@ class Team(AuthMixin, Base):
                     my_field="user_group_id",
                     other_field="id",
                 ),
+                "members": Relation(
+                    kind="many",
+                    other_type="Member",
+                    my_field="id",
+                    other_field="team_id"
+                )
             },
         )
- 
+
+class Member(AuthMixin, Base):
+    __tablename__ = "members"
+
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    first_name = sqla.Column(sqla.String(80), nullable=False)
+    last_name = sqla.Column(sqla.String(80), nullable=False)
+    permission_level = sqla.Column(sqla.String(10), nullable=False)
+    authorized_locations = sqla.Column(sqla.String(255), nullable=True)
+    contact_information = sqla.Column(sqla.String(255), nullable=True)
+    date_joined = sqla.Column(sqla.Date, nullable=False)
+    team_id = sqla.Column(sqla.ForeignKey("teams.id"), nullable=False)
+
+    @classmethod
+    def register_class(cls):
+        auth.register_class(
+            cls,
+            fields={
+                "team": Relation(
+                    kind="one",
+                    other_type="Team",
+                    my_field="team_id",
+                    other_field="id"
+                )
+            }
+        )
